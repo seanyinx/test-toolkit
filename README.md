bfcaa5247f7eca7420b6c908a9c715812efccb29
yinxiang <yinxiang@oneapm.com>
2017-04-02 18:31:10 +0800

    added a simple readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Test Toolkit
+Unit test scaffolding to help speed up test case writing
--- unit-scaffolding/src/main/java/io/seanyinx/github/unit/scaffolding/AssertUtils.java
+++ unit-scaffolding/src/main/java/io/seanyinx/github/unit/scaffolding/AssertUtils.java
@@ -3,6 +3,9 @@ package io.seanyinx.github.unit.scaffolding;
 import static org.junit.Assert.fail;
 
 public class AssertUtils {
+    private AssertUtils() {
+    }
+
     public static void expectFailing(Class<?> exceptionClass) {
         fail(exceptionClass + " expected, but none thrown");
     }
